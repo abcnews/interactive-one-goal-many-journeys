87f9d8338e5b3dcc89803b8dcea40e940ece3ad8
Joshua Byrd <phocks@gmail.com>
2026-04-30 16:46:45 +1000

    Don't global css stuff

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
-  let count: number = $state(0)
+  let count: number = $state(0);
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
 </script>
 
 <button type="button" class="counter" onclick={increment}>
   Count is {count}
 </button>
+
+<style>
+  .counter {
+    font-family: var(--mono);
+    display: inline-flex;
+    border-radius: 4px;
+    color: var(--text-h);
+  }
+
+  .counter {
+    font-size: 16px;
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: var(--accent);
+    background: var(--accent-bg);
+    border: 2px solid transparent;
+    transition: border-color 0.3s;
+    margin-bottom: 24px;
+
+    &:hover {
+      border-color: var(--accent-border);
+    }
+    &:focus-visible {
+      outline: 2px solid var(--accent);
+      outline-offset: 2px;
+    }
+  }
+</style>
