2d33b9f54ca16b2cc916268d940aa73c66975fc2
Joshua Byrd <phocks@gmail.com>
2026-05-08 10:06:13 +1000

    import meta polyfil

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import mkcert from "vite-plugin-mkcert";
 
+const importMetaUrlPolyfillVariableName = "__import_meta_url__";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), mkcert()],
@@ -11,9 +13,17 @@ export default defineConfig({
   },
   build: {
     rolldownOptions: {
+      transform: {
+        define: {
+          "import.meta.url": importMetaUrlPolyfillVariableName,
+        },
+      },
       output: {
         format: "iife",
         entryFileNames: "[name].js",
+        intro:
+          "var _documentCurrentScript = typeof document !== 'undefined' ? document.currentScript : null;" +
+          `var ${importMetaUrlPolyfillVariableName} = (_documentCurrentScript && _documentCurrentScript.tagName.toUpperCase() === 'SCRIPT' && _documentCurrentScript.src || new URL('main.js', document.baseURI).href)`,
       },
     },
   },
