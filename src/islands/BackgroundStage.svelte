a04e9973ea36863234bf667090c697a33824f1b9
Joshua Byrd <phocks@gmail.com>
2026-05-26 17:14:19 +1000

    Actually commit

---
+++ src/islands/BackgroundStage.svelte
@@ -0,0 +1,1 @@
+<div class="u-full">Hello World!</div>
---
+++ src/islands/StickyMount.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import Globe from "../components/Globe.svelte";
+  import Panels from "../components/Panels.svelte";
+</script>
+
+<div class="map u-full sticky-container">
+  <div class="sticky-block">
+    <Globe />
+  </div>
+  <div class="panels">
+    <Panels />
+  </div>
+</div>
+
+<style lang="scss">
+  :global {
+    #globe[data-mount="true"] {
+      padding-inline: 0;
+    }
+  }
+  .map {
+    :global(.maplibregl-map) {
+      height: 100dvh;
+    }
+  }
+  .u-full {
+    margin-block-end: 0;
+  }
+  .sticky-container {
+    min-height: 200dvh;
+  }
+  .sticky-block {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+  }
+  .panels {
+    position: relative;
+    z-index: 2;
+  }
+</style>
