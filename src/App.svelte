0331eea7c57b48152577782ebd8c3c8145494f4a
Joshua Byrd <phocks@gmail.com>
2026-06-01 22:59:01 +1000

    Current Panel detection on scroll

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { onMount, untrack } from "svelte";
   import { ScrollState, ElementSize, watch } from "runed";
+
   import { reducedMotion } from "@stores/reducedMotion.svelte";
+
   import Utils from "./components/Utils.svelte";
   import BackgroundStage from "./components/BackgroundStage.svelte";
   import LandingCollage from "./components/LandingCollage/LandingCollage.svelte";
@@ -14,17 +16,24 @@
   });
 
   let panelStates = $derived.by(() => {
-    // For reactivity mostly
-    if (!bodySize.height) return [];
+    if (!bodySize.height) return []; // For reactivity mostly
 
-    const panels = document.querySelectorAll('[data-key="panel"]');
+    const panels = document.querySelectorAll(
+      '[data-key^="panel"]',
+    ) as NodeListOf<HTMLElement>;
 
     return Array.from(panels).map((panel) => ({
+      name: panel.dataset.tag,
       top: panel.getBoundingClientRect().top + untrack(() => scroll.y),
     }));
   });
 
-  $inspect(panelStates);
+  const currentPanel = $derived(
+    panelStates.findLast((panel) => scroll.y + windowInnerHeight > panel.top) ??
+      null,
+  );
+
+  $inspect(currentPanel);
 
   // Start reactive observation of reduced motion toggle setting
   onMount(() => reducedMotion.observe());
